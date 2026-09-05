171609ef635cd47d60b5d3c721c27c47e8eab8b4
mrtinzinn <martin.hofer@alumnos.ucn.cl>
2026-09-05 16:51:18 -0400

    cambios nombres

--- taller1/taller01/Main.java
+++ taller1/taller01/Main.java
@@ -1,5 +1,6 @@
-//Martin Hofer Ochoa - 22.368.055-0 - ICCI
+//Martin Hofer Ochoa - 22.368.055-0 - mrtinzinn
 //Vicente Jara
+//https://github.com/mrtinzinn/Taller1_POO/tree/main
 
 package taller01;
 
@@ -7,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mi nombre es martin");
-
+		
 	}
 
 }
